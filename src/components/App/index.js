2406b642567849dbd7e3d54e76d6862e5f441cab
Victor Greiveldinger <VGrei@MacBook-Pro-de-Victor-3.local>
2020-11-09 17:18:41 +0100

    Clean Repo

---
+++ src/components/App/index.js
@@ -0,0 +1,18 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.css';
+
+// == Composant
+const App = () => {
+
+  return (
+    <div className="app">
+      Plop
+    </div>
+  );
+};
+
+// == Export
+export default App;
